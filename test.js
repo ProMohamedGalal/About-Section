f95f95028d4b6e4f9c48148ee9abbc737a48e22e
Pro.MohamedGalal <ProMohamedGalal@users.noreply.github.com>
2020-01-14 01:18:22 +0200

    Add files via upload

---
+++ test.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-unused-vars */
+/*jslint browser:true, evil: true */
+/*jslint sloppy:true, white:true, browser:true, regexp: true */
+/* eslint-env browser */
+/* eslint-env es6 */
+/* eslint-disable no-console */
+/* eslint-disable */
+/*global
+const, alert, confirm,  console, Debug, opera, prompt, WSH ,switch, case, console, $, continue, clearInterval, clearTimeout, document, event, FileReader, FormData, history, localStorage, location, name, navigator, screen, sessionStorage, setInterval, setTimeout, Storage, URL, window, XMLHttpRequest, emit, getRow, isArray, log, provides, registerType, require, send, start, sum, toJSON, Buffer, clearImmediate, clearInterval, clearTimeout, console, exports, module, process, require, setImmediate, setInterval, setTimeout, URL, URLSearchParams,
+*/
+/* eslint-disable no-console */
+/*jslint plusplus: true */
+
+
+
+
+
+
+
